975b4d40f311731576d23d145a415da5fe384adf
timcarey <timcarey1989@gmail.com>
2023-10-15 20:25:56 -0400

    get tiptap working for creating/editing notes

--- app/notes/[id]/page.jsx
+++ app/notes/[id]/page.jsx
@@ -2,10 +2,17 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import Form from 'components/Form'
 
 const MyNote = ({ params }) => {
-  const [note, setNote] = useState([]);
+  const [note, setNote] = useState({
+    title: '',
+    category: '',
+  })
+  const [submitting, setSubmitting] = useState(false)
   const router = useRouter()
+  const { data: session } = useSession()
 
   useEffect(() => {
     const fetchNote = async () => {
@@ -16,6 +23,32 @@ const MyNote = ({ params }) => {
 
     if (params?.id) fetchNote();
   }, [params.id]);
+
+  const editNote = async (e) => {
+    e.preventDefault()
+    setSubmitting(true)
+
+    try {
+      const res = await fetch(`/api/note/${params?.id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          text: note.text,
+          userId: session?.user.id,
+          title: note.title,
+          category: note.category
+        })
+      })
+
+      if (res.ok) {
+        router.push('/')
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setSubmitting(false)
+    }
+
+  }
 
   const handleDelete = async () => {
     const hasConfirmed = confirm("Are you sure you want to delete this note?")
@@ -36,9 +69,14 @@ const MyNote = ({ params }) => {
   return (
     <div>
       <p>{note.title}</p>
-      <textarea className="textarea textarea-ghost" value={note.text}></textarea>
-      <p>{note.category}</p>
-      <p>{note.date}</p>
+      {/* <textarea className="textarea textarea-ghost" value={note.text}></textarea> */}
+      <Form
+        type="Edit"
+        note={note}
+        setNote={setNote}
+        submitting={submitting}
+        handleSubmit={editNote}
+      />
       <button onClick={handleDelete} className="btn btn-error">Delete</button>
     </div>
   );
