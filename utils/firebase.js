21362dc8c0b76034311f54fb0fc20e6179d5f298
timcarey <timcarey1989@gmail.com>
2023-10-17 11:50:35 -0400

    changes

---
+++ utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "notes-app-402012.firebaseapp.com",
+  projectId: "notes-app-402012",
+  storageBucket: "notes-app-402012.appspot.com",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
